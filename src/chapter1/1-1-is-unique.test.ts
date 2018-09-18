1a2962ef5ffdc11e2c53dce99d1cb619a670249c
Cody <mizx@users.noreply.github.com>
2018-09-17 22:18:35 -0700

    updated test names for 1.1, added test

--- src/chapter1/1-1-is-unique.test.ts
+++ src/chapter1/1-1-is-unique.test.ts
@@ -2,15 +2,22 @@ import { isUnique, isUniqueBits } from "./1-1-is-unique";
 
 describe('Chapter 1.1: Is Unique', () => {
   describe('isUnique()', () => {
-    it('should return true', () => {
+    it('should return true when input does have unique characters', () => {
       const input = 'abc';
       const result = isUnique(input);
 
       expect(result).toBe(true);
     });
 
-    it('should return false', () => {
+    it('should return false when input does not have unique characters', () => {
       const input = 'abcc';
+      const result = isUnique(input);
+
+      expect(result).toBe(false);
+    });
+
+    it('should return false when input is larger than character set (ASCII)', () => {
+      const input = 'a'.repeat(200);
       const result = isUnique(input);
 
       expect(result).toBe(false);
