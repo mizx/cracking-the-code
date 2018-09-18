73c9f73d45e538ef8d6970791318e07b6cfe4b3f
Cody <mizx@users.noreply.github.com>
2018-09-17 19:36:49 -0700

    updated 1.1 to use array isntead of set

--- src/chapter1/1-1-is-unique.ts
+++ src/chapter1/1-1-is-unique.ts
@@ -1,15 +1,22 @@
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
 export const isUnique = (input: string) => {
-  const chars = new Set<string>();
+  if (input.length > 128) {
+    return false;
+  }
+  const chars = new Array(128);
 
   for (let i = 0; i < input.length; i++) {
-    const char = input[i];
+    const charCode = input.charCodeAt(i);
 
-    if (chars.has(char)) {
+    if (chars[charCode]) {
       return false;
     }
 
-    chars.add(char);
+    chars[charCode] = true;
   }
 
   return true;
-}
+};
