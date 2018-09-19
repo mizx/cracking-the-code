cf1d326558526cb09674625d826874f56e9061fa
Cody <mizx@users.noreply.github.com>
2018-09-18 23:31:34 -0700

    exercise 1.3

---
+++ src/chapter1/1-3-urlify.test.ts
@@ -0,0 +1,9 @@
+import { urlify } from "./1-3-urlify";
+
+describe('Chapter 1.3: URLify', () => {
+  it('should replace spaces with "%20"', () => {
+    const result = urlify('Mr John Smith    '.split(''), 13);
+    
+    expect(result).toBe('Mr%20John%20Smith');
+  });
+});
---
+++ src/chapter1/1-3-urlify.ts
@@ -0,0 +1,25 @@
+export const urlify = (input: string[], trueLength: number) => {
+  let spaceCount = 0;
+
+  for (let i = 0; i < trueLength; i++) {
+    if (input[i] === ' ') {
+      spaceCount++;
+    }
+  }
+
+  let index = trueLength + spaceCount * 2;
+
+  for (let i = trueLength - 1; i >= 0; i--) {
+    if (input[i] === ' ') {
+      input[index - 1] = '0';
+      input[index - 2] = '2';
+      input[index - 3] = '%'
+      index -= 3;
+    } else {
+      input[index - 1] = input[i];
+      index -= 1;
+    }
+  }
+
+  return input.join('');
+}
